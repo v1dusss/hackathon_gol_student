c31169de4529b88ca911db52416d5d83e2cb4c60
Moritz Falk <Moritz@falkhf.de>
2025-09-29 15:44:59 +0200

    build clean up

--- build.bat
+++ build.bat
@@ -1,5 +1,4 @@
 @echo off
-REM cl /c /nologo life_simulate.c /O2 /Zi /Wall /WX /link life_renderer.obj life_simulate.obj /DEBUG:FULL
-cl /c /nologo life_simulate.c /O2 /Zi /Wall /WX /DEBUG:FULL
+cl /c /nologo life_simulate.c /Od /Zi /Wall /WX /DEBUG:FULL
 
 link life_main.obj life_renderer.obj life_simulate.obj
